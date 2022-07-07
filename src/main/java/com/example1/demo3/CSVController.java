2dc1632bb075bc4b204c3d4b0fc8c1e1aea3df31
venkeykfl <venkatesh.jonnakuti@gmail.com>
2022-07-07 23:28:49 +0530

    added files

--- src/main/java/com/example1/demo3/CSVController.java
+++ src/main/java/com/example1/demo3/CSVController.java
@@ -1,4 +1,5 @@
 package com.example1.demo3;
+
 import java.io.File;
 import java.io.IOException;
 import org.apache.commons.io.FileUtils;
@@ -12,10 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-
 @RestController
 public class CSVController {
-	
+
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return "Hello World Welcome";
@@ -23,36 +23,43 @@ public class CSVController {
 
 	@GetMapping("/jsonObj")
 	public String testJson(@RequestBody String jsonObj) throws JSONException, IOException {
-		
+
 		String status = jsonToCvConversion(jsonObj);
-		
+
 		return status;
 	}
-	
+
 	@GetMapping("/jsonTocv")
-	public String jsonTocv(@RequestParam String jsonUrl) throws JSONException, IOException {
+	public String jsonTocv(@RequestParam String jsonUrl) throws JSONException, IOException, CustomException {
 		final String uri = jsonUrl;
-	    RestTemplate restTemplate = new RestTemplate();
-	    String jsonObj = restTemplate.getForObject(uri, String.class);
+		RestTemplate restTemplate = new RestTemplate();
+		String jsonObj = restTemplate.getForObject(uri, String.class);
 		String status = jsonToCvConversion(jsonObj.toString());
 		return status;
 	}
+
 	private String jsonToCvConversion(String jsonObj) throws IOException, JSONException {
 		JSONObject output;
-		// TODO Auto-generated method stub
+		String status = null;
 		char firstChar = jsonObj.trim().charAt(0);
-		if(String.valueOf(firstChar).equalsIgnoreCase("{")) {
-			String jsonarrayValue = jsonObj.substring(1, jsonObj.indexOf(":")).substring(0).replace("{", "").trim();
-			String arrayName = jsonObj.replaceFirst(jsonarrayValue, "data");
-			output = new JSONObject(arrayName);
-			JSONArray docs = output.getJSONArray("data");
-			File file = new File("D:/jsonObj/jsonTocv.csv");
-			String csv = CDL.toString(docs);
-			FileUtils.writeStringToFile(file, csv); 
-			return "success";
+		try {
+			if (String.valueOf(firstChar).equalsIgnoreCase("{")) {
+				String jsonarrayValue = jsonObj.substring(1, jsonObj.indexOf(":")).substring(0).replace("{", "").trim();
+				String arrayName = jsonObj.replaceFirst(jsonarrayValue, "data");
+				output = new JSONObject(arrayName);
+				JSONArray docs = output.getJSONArray("data");
+				File file = new File("D:/jsonObj/jsonTocv.csv");
+				String csv = CDL.toString(docs);
+				FileUtils.writeStringToFile(file, csv);
+				status = "success";
+				return status;
+			} else {
+				status = " its not a json array";
+				return status;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		else {
-			return "its not json array";
-		}
+		return status;
 	}
 }
---
+++ src/main/java/com/example1/demo3/CustomException.java
@@ -0,0 +1,14 @@
+package com.example1.demo3;
+
+public class CustomException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String urlDataEmpty() {
+		return "empty Data";
+	}
+
+}
